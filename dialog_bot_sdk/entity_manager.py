118c0b4a75af5a6c8e813077f7f8a67b4db8b8ec
Andrey Skiba <a.skiba@dlg.im>
2018-12-27 14:15:07 +0300

    fix(python): create correct project structure

--- dialog_bot_sdk/entity_manager.py
+++ dialog_bot_sdk/entity_manager.py
@@ -1,9 +1,5 @@
 from .internal.peers import private_peer, group_peer, peer_hasher
-import dialog_api
-if dialog_api.PATH_WORKAROUND:
-    import peers_pb2, messaging_pb2, miscellaneous_pb2, contacts_pb2
-else:
-    from dialog_api import peers_pb2, messaging_pb2, miscellaneous_pb2, contacts_pb2
+from dialog_api import peers_pb2, messaging_pb2, miscellaneous_pb2, contacts_pb2
 
 DEFAULT_OPTIMIZATIONS = [
     miscellaneous_pb2.UPDATEOPTIMIZATION_STRIP_ENTITIES,
