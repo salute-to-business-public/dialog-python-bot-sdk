0f5695229bbea2d3445ea43892bf6e77fd02f6cc
Andrey Skiba <a.skiba@dlg.im>
2019-01-31 13:49:28 +0300

    feat(python): handling users custom profile field

--- dialog_bot_sdk/users.py
+++ dialog_bot_sdk/users.py
@@ -1,5 +1,5 @@
 from .service import ManagedService
-from dialog_api import contacts_pb2, peers_pb2
+from dialog_api import contacts_pb2, peers_pb2, users_pb2
 
 
 class Users(ManagedService):
@@ -19,3 +19,38 @@ class Users(ManagedService):
                 )
                 return outpeer
         return None
+
+    def get_user_by_nick(self, nick):
+        users = self.internal.contacts.SearchContacts(
+            contacts_pb2.RequestSearchContacts(
+                request=nick
+            )
+        ).users
+
+        for user in users:
+            if user.nick.value == nick:
+                return user
+
+    def get_user_full_profile(self, nick):
+        user = self.find_user_outpeer_by_nick(nick)
+        full_profile = self.internal.users.LoadFullUsers(
+            users_pb2.RequestLoadFullUsers(
+                user_peers=[
+                    peers_pb2.UserOutPeer(
+                        uid=user.id,
+                        access_hash=user.access_hash
+                    )
+                ]
+            )
+        )
+
+        if full_profile:
+            if hasattr(full_profile, 'full_users'):
+                if len(full_profile.full_users) > 0:
+                    return full_profile.full_users[0]
+
+    def get_user_custom_profile(self, nick):
+        full_profile = self.get_user_full_profile(nick)
+
+        if hasattr(full_profile, 'custom_profile'):
+            return str(full_profile.custom_profile)
